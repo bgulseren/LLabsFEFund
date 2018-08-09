e31f655713b920e25f84dc1b4819d98f79024a5f
Burak Gulseren <BGulseren@gnthermoforming.onmicrosoft.com>
2018-08-09 20:48:41 -0300

    Adding W5D2 in class assignments. Better to be done in functions for better organization. Watch the video W5D2 in order to see how Andrew did.

--- jQuery Practices/W5D1/index.js
+++ jQuery Practices/W5D1/index.js
@@ -1,9 +1,41 @@
+var horoscopeData = {
+  "sunsign": "Gemini",
+  "credit": "(c) Kelli Fox, The Astrologer, http://new.theastrologer.com",
+  "date": "2018-03-09",
+  "horoscope": "The world is your oyster today. So what are you waiting for? Get out there and find your adventure. It's a perfect day for learning something new. This can be formalized through taking a class, or it can be an informal, impromptu process. That's what 'the world is your oyster' means -- your pearl of knowledge is out there, just waiting to be discovered. So where will you search for it first?(c) Kelli Fox, The Astrologer, http://new.theastrologer.com",
+  "meta": {
+      "mood": "strange",
+      "keywords": "one-sided, fervor",
+      "intensity": "87%"
+  }
+}
+
+var searchWord = "";
+
 $(document).ready(function() {
   // WRITE YOUR CODE HERE.
   $('form').on('submit', function(event) {
     event.preventDefault();
+    searchWord = $('input').val(); //collects the search keyword.
     $('form').hide(200);
-    $('#searchresults').fadeIn(2000);
+    //$('#searchresults').fadeIn(2000);
+
+    //jquery show, fades plays with display attribute.
+
+    $('#searchresults').removeClass('noshow');
+    $('#searchresults').addClass('flex');
+
+    // We are putting this function call here as if check should happen after submit.
+    if (searchWord === horoscopeData.sunsign) {
+      $('#searchresults').append("<p>" + "Credit: " + horoscopeData.credit + "</p>");
+      $('#searchresults').append("<p>" + "Date: " + horoscopeData.date + "</p>");
+      $('#searchresults').append("<p>" + horoscopeData.horoscope + "</p>");
+      $('#searchresults').append("<p>" + "Mood: " + horoscopeData.meta.mood + "</p>");
+      $('#searchresults').append("<p>" + "Keywords: " + horoscopeData.meta.keywords + "</p>");
+      $('#searchresults').append("<p>" + "Intensity: " + horoscopeData.meta.intensity + "</p>");
+    } else {
+      $('#searchresults').append("<p>" + "Sorry! Sign not found" + "</p>");
+    }
   });
 
   $('form').children().on('focus', function() {
@@ -15,3 +47,11 @@ $(document).ready(function() {
   });
 
 });
+
+$('#again').on('click', function(event) {
+  $('#searchresults').removeClass('flex');
+  $('#searchresults').addClass('noshow');
+  $('form').fadeIn(1000);
+});
+
+
